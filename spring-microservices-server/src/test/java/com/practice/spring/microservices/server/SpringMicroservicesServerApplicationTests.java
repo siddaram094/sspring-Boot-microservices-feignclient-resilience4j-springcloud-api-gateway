3999d3e7bdd0f3ba948e570405e739d8cf0a1078
siddaram094 <70060254+siddaram094@users.noreply.github.com>
2023-02-25 22:06:36 +0530

    Add files via upload

---
+++ spring-microservices-server/src/test/java/com/practice/spring/microservices/server/SpringMicroservicesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spring.microservices.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
